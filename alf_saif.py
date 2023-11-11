7ae00f3547f0c1a55933096b20b1c87859aa5b25
WebAnLiMaks studio <78268817+evembar@users.noreply.github.com>
2023-11-12 01:50:23 +0400

    Add files via upload

--- alf_saif.py
+++ alf_saif.py
@@ -1,10 +1,12 @@
 from aiogram import Bot, Dispatcher, executor, types
 import time
 from aiogram.types import BotCommand
-import asyncio 
+import asyncio
 import aioschedule
+from datetime import datetime, timedelta, timezone
 
-API_TOKEN = '*****your token******'
+API_TOKEN = '***************YOUR_API***************'
+
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
@@ -14,21 +16,25 @@ maxstickerblock = 25
 blockedstickerblock = 0
 continuer = 0
 
-photoblock = 0
-maxphotoblock = 25
-blockedphotoblock = 0
+
+#your utc region
+utc = 1
+
+hoster = 'WebMast'
 
 gifblock = 0
 maxgifblock = 25
 blockedgifblock = 0
 
-videoblock = 0
-maxvideoblock = 15
-blockedvideoblock = 0
+user_track_spam = ''
+new_message_time = 0.0
+old_message_time = 0.0
+stop_spam_sticker = 'CAACAgQAAxkBAAEB67RlT7UCv46lCb1QULxPKEZp6FxKDgAC2AwAApzYQFLmTLCt5d5_tDME'
+spam_sticker_seconds = 0
 
 @dp.message_handler(commands=['about'], content_types=['any'])
 async def startup_message(message: types.Message):
-   info_msg = await message.answer('Малик Альф Саиф. Версия 0.3. Made by WebMast from WebAnLiMaks studio, hosted by Nikita44')
+   info_msg = await message.answer(f'Малик Альф Саиф. Версия 0.04. Made by WebMast from WebAnLiMaks studio, hosted by {hoster}')
    video_msg = await message.answer_photo(r'https://github.com/evembar/malik_bot/raw/main/startup.webp')
    time_set = time.time()
    local_time = time.ctime(time_set)
@@ -53,44 +59,14 @@ async def startup_message(message: types.Message):
    await message.delete()
    log.write(f'\n {local_time} : {user_sticker} вызвал статистику. Доступных стикеров осталось:  {maxstickerblock - stickerblock}. Заблокированных : {blockedstickerblock}')
    log.close()
-   print(f'\nДоступных стикеров осталось:  {maxstickerblock - stickerblock}. Заблокированных : {blockedstickerblock}')
-   time.sleep(2)
-   await static.delete()
-
-@dp.message_handler(commands=['photo'], content_types=['any'])
-async def startup_message(message: types.Message):
-   global photoblock, maxphotoblock, blockedphotoblock
-   static = await message.answer(f'Доступных картинок осталось:  {maxphotoblock - photoblock}. Заблокированных : {blockedphotoblock}')
-   time_set = time.time()
-   local_time = time.ctime(time_set)
-   log = open('log.txt', 'a')
-   user_sticker = message.from_user.first_name
-   await message.delete()
-   log.write(f'\n {local_time} : {user_sticker} вызвал статистику. Доступных картинок осталось:  {maxphotoblock - photoblock}. Заблокированных : {blockedphotoblock}')
-   log.close()
-   print(f'\n Доступных картинок осталось:  {maxphotoblock - photoblock}. Заблокированных : {blockedphotoblock}')
-   time.sleep(2)
-   await static.delete()
-
-@dp.message_handler(commands=['video'], content_types=['any'])
-async def startup_message(message: types.Message):
-   global videoblock, maxvideoblock, blockedvideoblock
-   static = await message.answer(f'Доступных видосов осталось:  {maxvideoblock - videoblock}. Заблокированных : {blockedvideoblock}')
-   time_set = time.time()
-   local_time = time.ctime(time_set)
-   log = open('log.txt', 'a')
-   user_sticker = message.from_user.first_name
-   await message.delete()
-   log.write(f'\n {local_time} : {user_sticker} вызвал статистику. Доступных видосов осталось:  {maxvideoblock - videoblock}. Заблокированных : {blockedvideoblock}')
-   log.close()
-   print(f'\n Доступных видосов осталось:  {maxvideoblock - videoblock}. Заблокированных : {blockedvideoblock}')
+   print(f'\n {local_time} : Доступных стикеров осталось:  {maxstickerblock - stickerblock}. Заблокированных : {blockedstickerblock}')
    time.sleep(2)
    await static.delete()
 
 @dp.message_handler(commands=['gif'], content_types=['any'])
 async def startup_message(message: types.Message):
    global gifblock, maxgifblock, blockedgifblock
-   static = await message.answer(f'Доступных гифок осталось:  {maxgifblock - gifblock}. Заблокированных : {blockedgifblock}')
+   static = await message.answer(f'{local_time} : Доступных гифок осталось:  {maxgifblock - gifblock}. Заблокированных : {blockedgifblock}')
    time_set = time.time()
    local_time = time.ctime(time_set)
    log = open('log.txt', 'a')
@@ -98,7 +74,7 @@ async def startup_message(message: types.Message):
    await message.delete()
    log.write(f'\n {local_time} : {user_sticker} вызвал статистику. Доступных гифок осталось:  {maxgifblock - gifblock}. Заблокированных : {blockedgifblock}')
    log.close()
-   print(f'\n Доступных гифок осталось:  {maxgifblock - gifblock}. Заблокированных : {blockedgifblock}')
+   print(f'\n {local_time} : Доступных гифок осталось:  {maxgifblock - gifblock}. Заблокированных : {blockedgifblock}')
    time.sleep(2)
    await static.delete()
    
@@ -232,7 +208,7 @@ async def startup_message(message: types.Message):
 
 @dp.message_handler(commands=['start']) #Явно указываем в декораторе, на какую команду реагируем. 
 async def send_welcome(message: types.Message):
-   info_start_msg = await message.reply("Я Малик Альф-Саиф и если не будешь следовать правилам, то я наведу такую трёпку\nВ частности, если вы будете спаммить стикерами, то каждого буду банить в зависимости, сколько решат поставить времени.\n Пока WebMastом(Виктором Яланжи) реализовывается, так что вам не долго осталось радоваться. >:)  ")
+   info_start_msg = await message.reply("Я Малик Альф-Саиф и если не будешь следовать правилам, то я наведу такую трёпку\nВ частности, если вы будете спаммить стикерами, то каждого буду банить в зависимости, сколько решат поставить времени.\n WebMast уже реализовал это, приятного общения!!!. >:)  ")
 
    time_set = time.time()
    local_time = time.ctime(time_set)
@@ -246,47 +222,41 @@ async def send_welcome(message: types.Message):
    await info_start_msg.delete()
 
 async def obnull_limit_rasp():
-   global stickerblock, videoblock, gifblock, photoblock
+   global stickerblock,  gifblock
 
    stickerblock = 0
-   videoblock = 0
    gifblock = 0
-   photoblock = 0
 
    time_set = time.time()
    local_time = time.ctime(time_set)
    log = open('log.txt', 'a')
-   log.write(f'\n {local_time}: Обнуление лимита стикеров, видео, гифок, фоток')
+   log.write(f'\n {local_time}: Обнуление лимита стикеров,гифок')
    log.close()
+   print(f'\n {local_time}: Обнуление лимита стикеров,гифок')
 
 async def scheduler():
-    aioschedule.every().day.at("0:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("1:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("2:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("3:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("4:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("5:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("6:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("7:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("8:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("9:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("10:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("11:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("12:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("13:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("14:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("15:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("16:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("17:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("18:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("19:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("20:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("21:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("22:00").do(obnull_limit_rasp)
-    aioschedule.every().day.at("23:00").do(obnull_limit_rasp)
     while True:
-        await aioschedule.run_pending()
-        await asyncio.sleep(1)
+         await aioschedule.run_pending()
+
+         timezone = datetime(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour, datetime.now().minute, datetime.now().second)
+
+         if (timezone.hour == 0 and timezone.minute == 0 and timezone.second == 0) or (timezone.hour == 1 and timezone.minute == 0 and timezone.second == 0) or (timezone.hour == 2 and timezone.minute == 0 and timezone.second == 0) or (timezone.hour == 3 and timezone.minute == 0 and timezone.second == 0):
+           await obnull_limit_rasp()
+         elif (timezone.hour == 4 and timezone.minute == 0 and timezone.second == 0) or (timezone.hour == 5 and timezone.minute == 0 and timezone.second == 0) or (timezone.hour == 6 and timezone.minute == 0 and timezone.second == 0) or (timezone.hour == 7 and timezone.minute == 0 and timezone.second == 0):
+           await obnull_limit_rasp()
+         elif (timezone.hour == 8 and timezone.minute == 0 and timezone.second == 0) or (timezone.hour == 9 and timezone.minute == 0 and timezone.second == 0) or (timezone.hour == 10 and timezone.minute == 0 and timezone.second == 0) or (timezone.hour == 11 and timezone.minute == 0 and timezone.second == 0):
+           await obnull_limit_rasp()
+         elif (timezone.hour == 12 and timezone.minute == 0 and timezone.second == 0) or (timezone.hour == 13 and timezone.minute == 0 and timezone.second == 0) or (timezone.hour == 14 and timezone.minute == 0 and timezone.second == 0) or (timezone.hour == 15 and timezone.minute == 0 and timezone.second == 0):
+           await obnull_limit_rasp()
+         elif (timezone.hour == 16 and timezone.minute == 0 and timezone.second == 0) or (timezone.hour == 17 and timezone.minute == 0 and timezone.second == 0) or (timezone.hour == 18 and timezone.minute == 0 and timezone.second == 0) or (timezone.hour == 19 and timezone.minute == 0 and timezone.second == 0):
+           await obnull_limit_rasp()
+         elif (timezone.hour == 20 and timezone.minute == 0 and timezone.second == 0) or (timezone.hour == 21 and timezone.minute == 0 and timezone.second == 0) or (timezone.hour == 22 and timezone.minute == 0 and timezone.second == 0) or (timezone.hour == 23 and timezone.minute == 0 and timezone.second == 0):
+           await obnull_limit_rasp()
+         else:
+            pass
+
+         await asyncio.sleep(1)
+        
 
 
 async def setup_default_commands(dp):
@@ -299,17 +269,14 @@ async def setup_default_commands(dp):
         BotCommand(command="obnull_limit_all", description="Обнуление лимита отправляемых медиа"),
         BotCommand(command="clear_log", description="Очистка логов"),
         BotCommand(command="sticker", description="Проверка доступных к отправке стикеров"),
-        BotCommand(command="photo", description="Проверка доступных к отправке картинок"),
         BotCommand(command="gif", description="Проверка доступных к отправке гифок"),
-        BotCommand(command="video", description="Проверка доступных к отправке видео"),
     ])
     asyncio.create_task(scheduler())
       
 
 @dp.message_handler(content_types=['any']) #Создаём новое событие, которое запускается в ответ на любой текст, введённый пользователем.
 async def echo(message: types.Message): #Создаём функцию с простой задачей — отправить обратно тот же текст, что ввёл пользователь.
-   global stickerblock, maxstickerblock, blockedstickerblock, photoblock, maxphotoblock, blockedphotoblock, gifblock, maxgifblock, blockedgifblock
-   global videoblock, maxvideoblock, blockedvideoblock
+   global stickerblock, maxstickerblock, blockedstickerblock,gifblock, maxgifblock, blockedgifblock, user_track_spam, new_message_time, old_message_time, spam_sticker_seconds
    if message.sticker:
       if message.chat.type == 'private':
          await message.answer('Не думай, что ты можешь меня обхитрить, ибо я только в группах навожу порядок. Спамь сколько хочешь тут!')
@@ -323,6 +290,51 @@ async def echo(message: types.Message): #Создаём функцию с про
             stickerblock +=1
             status = 'НЕОтказано'
 
+         timemsg = datetime(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour, datetime.now().minute, datetime.now().second, datetime.now().microsecond)
+
+         if user_track_spam == '':
+            user_track_spam = message.from_user.first_name
+            print(user_track_spam)
+            new_message_time == timemsg.microsecond
+            old_message_time = ''
+         else:
+            if user_track_spam == message.from_user.first_name:
+               if new_message_time == '':
+                  new_message_time == timemsg.microsecond
+                  print(f'Принято стикерко за {new_message_time}')
+               else:
+                  old_message_time = new_message_time
+                  new_message_time = timemsg.microsecond
+                  print(f'Принято стикерко за {new_message_time}')
+                  if new_message_time < 400000 and old_message_time < 400000 :
+                     spam_sticker_seconds += 1
+                     print(f'Быстрый стикер: {spam_sticker_seconds} из 3')
+                  else:
+                     new_message_time == timemsg.microsecond
+                     old_message_time = ''
+                     spam_sticker_seconds = 0
+                     print("Медленный стикер")
+
+                  if spam_sticker_seconds == 3:
+                     #antispam_message_sticker = await message.answer_sticker(stop_spam_sticker)
+                     antispam_message_text = await message.answer(f'Критичная спам атака, происходит блокировка спаммера {user_track_spam}. Блокировка будет длиться в течении 15 минут')
+                     ban_time= (int(time.time()) + 900*utc+900)
+                     print(ban_time)
+
+                     await bot.restrict_chat_member(chat_id=message.chat.id, user_id=message.from_user.id,until_date=ban_time, can_send_messages=False)
+                     
+                     print(ban_time)
+                     
+                     asyncio.sleep(1)
+                     #antispam_message_sticker.delete()
+                     await antispam_message_text.delete()
+            else:
+               user_track_spam = message.from_user.first_name
+               print(user_track_spam)
+               print(2)
+               new_message_time == timemsg.microsecond
+               old_message_time = ''
+
          time_set = time.time()
          local_time = time.ctime(time_set)
          log = open('log.txt', 'a')
@@ -330,26 +342,8 @@ async def echo(message: types.Message): #Создаём функцию с про
          log.write(f'\n {local_time}: Пользователь {user_sticker} Отправил стикер. Статус - {status}')
          log.close()
          print(f'\n {local_time}: Пользователь {user_sticker} Отправил стикер. Статус - {status}')
-   elif message.photo:
-      if message.chat.type == 'private':
-         await message.answer('Как буд-то мне сдались твои порнокартинки')
-      else: 
-         
-         if photoblock == maxphotoblock:
-            await message.delete()
-            blockedphotoblock +=1
-            status = 'Отказано'
-         else:
-            photoblock +=1
-            status = 'НЕОтказано'
 
-         time_set = time.time()
-         local_time = time.ctime(time_set)
-         log = open('log.txt', 'a')
-         user_sticker = message.from_user.first_name
-         log.write(f'\n {local_time}: Пользователь {user_sticker} Отправил картинку. Статус - {status}')
-         log.close()
-         print(f'\n {local_time}: Пользователь {user_sticker} Отправил картинку. Статус - {status}')
+
    elif message.animation:
       if message.chat.type == 'private':
          await message.answer('Гифки и прочее дерьмо, как этим вы пользуетесь, малыши?')
@@ -370,30 +364,10 @@ async def echo(message: types.Message): #Создаём функцию с про
          log.write(f'\n {local_time}: Пользователь {user_sticker} Отправил гифку. Статус - {status}')
          log.close()
          print(f'\n {local_time}: Пользователь {user_sticker} Отправил гифку. Статус - {status}')
-   elif message.video:
-      if message.chat.type == 'private':
-         await message.answer('Не бось, это видео с участием мамки?')
-      else: 
-         
-         if videoblock == maxvideoblock:
-            await message.delete()
-            blockedvideoblock +=1
-            status = 'Отказано'
-         else:
-            videoblock +=1
-            status = 'НЕОтказано'
-
-         time_set = time.time()
-         local_time = time.ctime(time_set)
-         log = open('log.txt', 'a')
-         user_sticker = message.from_user.first_name
-         log.write(f'\n {local_time}: Пользователь {user_sticker} Отправил видос. Статус - {status}')
-         log.close()
-         print(f'\n {local_time}: Пользователь {user_sticker} Отправил видос. Статус - {status}')
    else:
       global continuer
       if continuer == 1:
-         await message.answer('Продолжай.')
+         cont_msg = await message.answer('Продолжай.')
          time_set = time.time()
          local_time = time.ctime(time_set)
          log = open('log.txt', 'a')
@@ -401,10 +375,12 @@ async def echo(message: types.Message): #Создаём функцию с про
          log.write(f'\n {local_time}: Пользователь {user_sticker} Предложил мне.')
          log.close()
          print(f'\n {local_time}: Пользователь {user_sticker} Предложил мне.')
+         asyncio.sleep(1)
+         cont_msg.delete()
       else:
          pass
 
 if __name__ == '__main__':
    
-   print('Запуск Malik Alf-Salif 0.3')
+   print('Запуск Malik Alf-Salif 0.04')
    executor.start_polling(dp, skip_updates=True, on_startup=setup_default_commands)
